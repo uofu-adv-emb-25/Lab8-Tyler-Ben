3c00f8ce4a0ef056b0d329e0ad78d4954242252b
Tyler <gourley18@gmail.com>
2025-10-27 13:05:55 -0600

    Remove ignored files from repository

--- build/src/CMakeFiles/hello_freertos.dir/home/tyler/ece6785/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/hello_freertos.dir/home/tyler/ece6785/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o: \
- /home/tyler/ece6785/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
