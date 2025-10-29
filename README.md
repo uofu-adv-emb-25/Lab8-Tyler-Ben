85e023e73736f9eb7c9d7ec59d457018affe37f5
Tyler <gourley18@gmail.com>
2025-10-29 14:08:33 -0600

    Work in Progress build errors

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# Renode setup
-The Raspberry Pico needs configuration files for Renode to work properly.
+# Lab 8: CAN Bus
 
-* On MacOS, the installation location is `/Applications/Renode.app/Contents/MacOs`
-* On Linux, the location for Debian, Fedora, and Arch is `/opt/renode`
-* On Windows, the location is `C://Program Files/Renode`
 
-To add the Pico configuration files:
-1. Copy `rp2040_spinlock.py` and `rp2040_divider.py` to the `scripts/pydev` directory of your Renode installation.
-1. Copy `rpi_pico_rp2040_w.repl` to the `platforms/cpus` directory.
+# Pins of the CAN
+Pin 1: Txd
+Pin 2: Ground
+Pin 3: Power
+Pin 4: Rxd
+Pin 5: Vio
+Pin 6: CANL
+Pin 7: CANH
+Pin 8: STBY
+
+# Wiring:
+    CAN       >  MICROCONTROLLER
+Pin 1: Txd    >  GPIO5 (PIN 7)
+Pin 2: Ground >  ground
+Pin 3: Power  >  5V
+Pin 4: Rxd    >  GPIO4 (PIN 6)
+Pin 5: Vio    >  3V3
+Pin 6: CANL   >  CANL
+Pin 7: CANH   >  CANH
+Pin 8: STBY   >  ground
+
+
+
---
+++ src/transmitter.c
@@ -0,0 +1,63 @@
+#include <can2040.h>
+#include <hardware/regs/intctrl.h>
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+// Transmitter Code
+
+static struct can2040 cbus;
+
+static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
+{
+    printf("sent a message");
+}
+
+static void PIOx_IRQHandler(void)
+{
+    can2040_pio_irq_handler(&cbus);
+}
+
+void canbus_setup(void)
+{
+    uint32_t pio_num = 0;
+    uint32_t sys_clock = 125000000, bitrate = 500000;
+    uint32_t gpio_rx = 4, gpio_tx = 5;
+
+    // Setup canbus
+    can2040_setup(&cbus, pio_num);
+    can2040_callback_config(&cbus, can2040_cb);
+
+    // Enable irqs
+    irq_set_exclusive_handler(PIO0_IRQ_0, PIOx_IRQHandler);
+    irq_set_priority(PIO0_IRQ_0, PICO_DEFAULT_IRQ_PRIORITY - 1);
+    irq_set_enabled(PIO0_IRQ_0, 1);
+
+    // Start canbus
+    can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
+}
+
+void transmit_task(__unused void *args)
+{
+    struct can2040_msg msg;
+        msg.data = 0;
+        msg.dlc = 8;
+        msg.id = 1;
+    while(1)
+    {
+        msg->data = msg->data + 1;
+
+        can2040_transmit(&cbus, &msg);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+int main(void)
+{
+    stdio_init_all();
+    canbus_setup();
+    TaskHandle_t task;
+    xTaskCreate(transmit_task, "MainThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1UL, &task);
+    vTaskStartScheduler();
+    return 0;
+}
