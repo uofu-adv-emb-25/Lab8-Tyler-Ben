51fa238026aff4916d4c0b6aa079a52030da709a
Bean07 <ben.martins537@gmail.com>
2025-10-31 15:20:23 -0600

    Attempting to debug, will merge.

--- src/main.c
+++ src/main.c
@@ -13,12 +13,13 @@ QueueHandle_t msg_queue;
 
 // ID 0 is data transmitter
 // ID 1 is noise transmitter
-uint8_t PICO_ID = 1;
+uint8_t PICO_ID = 0;
 
 static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
 {
+    printf("HERE, notify: %d, CAN2040_NOTIFY_TX: %d\n", notify, CAN2040_NOTIFY_TX);
     if (notify == CAN2040_NOTIFY_RX) {
-        xQueueSendToBack(msg_queue, msg, portMAX_DELAY);
+        xQueueSendToBackFromISR(msg_queue, msg, NULL);
     }
     else if (notify == CAN2040_NOTIFY_TX) {
         printf("sent a message\n");
@@ -39,11 +40,11 @@ void transmit_noise_task(__unused void *args)
     while(1)
     {
         msg.data32[1] = msg.data32[1] * 67;
-        if (msg.data[1] == 0) msg.data[1] = 1;  
+        if (msg.data32[1] == 0) msg.data32[1] = 1;  
         int Tstatus = can2040_transmit(&cbus, &msg);
         if(Tstatus < 0)
         {
-            printf("Failed to add message to buffer from noise, with data %d", msg.data32[1]);
+            //printf("Failed to add message to buffer from noise, with data %d\n", msg.data32[1]);
         }
     }
 }
@@ -57,12 +58,12 @@ void transmit_data_task(__unused void *args)
     while(1)
     {
         msg.data32[1] = 12345;
-        if (msg.data[1] == 0) msg.data[1] = 1;  
         int Tstatus = can2040_transmit(&cbus, &msg);
         if(Tstatus < 0)
         {
-            printf("Failed to add message to buffer from noise, with data %d", msg.data32[1]);
+            printf("Failed to add message to buffer from noise, with data %d\n", msg.data32[1]);
         }
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
@@ -91,7 +92,7 @@ void receive_task(__unused void *args)
         struct can2040_msg data;
         xQueueReceive(msg_queue, &data, portMAX_DELAY);
         printf("Got message: %d\n", data.data32[1]);
-        if(data.data32[1] == 12345)printf("Got the real data***************************************");
+        if(data.data32[1] == 12345)printf("Got the real data***************************************\n");
     }
 }
 
@@ -100,6 +101,7 @@ int main(void)
     stdio_init_all();
     msg_queue = xQueueCreate(100, sizeof(struct can2040_msg));
     canbus_setup();
+    sleep_ms(5000);
     TaskHandle_t trans_noise_task_h;
     TaskHandle_t trans_data_task_h;
     TaskHandle_t receive_task_h;
