3511e41e056694d47b8466afef728b7875bd0dee
Bean07 <ben.martins537@gmail.com>
2025-10-31 14:40:34 -0600

    First iteration of the dual transmitter and reciever code.

--- src/main.c
+++ src/main.c
@@ -6,20 +6,64 @@
 #include <task.h>
 #include <queue.h>
 
-// Reciever Code
+// Transmit and Recieve Code
 
 static struct can2040 cbus;
 QueueHandle_t msg_queue;
 
+// ID 0 is data transmitter
+// ID 1 is noise transmitter
+uint8_t PICO_ID = 1;
 
 static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
 {
-    xQueueSendToBack(msg_queue, msg, portMAX_DELAY);
+    if (notify == CAN2040_NOTIFY_RX) {
+        xQueueSendToBack(msg_queue, msg, portMAX_DELAY);
+    }
+    else if (notify == CAN2040_NOTIFY_TX) {
+        printf("sent a message\n");
+    }
 }
 
 static void PIOx_IRQHandler(void)
 {
     can2040_pio_irq_handler(&cbus);
+}
+
+void transmit_noise_task(__unused void *args)
+{
+    struct can2040_msg msg;
+    msg.data32[0] = 1;
+    msg.dlc = 8;
+    msg.id = 1;
+    while(1)
+    {
+        msg.data32[1] = msg.data32[1] * 67;
+        if (msg.data[1] == 0) msg.data[1] = 1;  
+        int Tstatus = can2040_transmit(&cbus, &msg);
+        if(Tstatus < 0)
+        {
+            printf("Failed to add message to buffer from noise, with data %d", msg.data32[1]);
+        }
+    }
+}
+
+void transmit_data_task(__unused void *args)
+{
+    struct can2040_msg msg;
+    msg.data32[0] = 1;
+    msg.dlc = 8;
+    msg.id = 1;
+    while(1)
+    {
+        msg.data32[1] = 12345;
+        if (msg.data[1] == 0) msg.data[1] = 1;  
+        int Tstatus = can2040_transmit(&cbus, &msg);
+        if(Tstatus < 0)
+        {
+            printf("Failed to add message to buffer from noise, with data %d", msg.data32[1]);
+        }
+    }
 }
 
 void canbus_setup(void)
@@ -41,12 +85,13 @@ void canbus_setup(void)
     can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
 }
 
-void main_task(__unused void *args)
+void receive_task(__unused void *args)
 {
     while(1) {
         struct can2040_msg data;
         xQueueReceive(msg_queue, &data, portMAX_DELAY);
-        printf("Got message\n");
+        printf("Got message: %d\n", data.data32[1]);
+        if(data.data32[1] == 12345)printf("Got the real data***************************************");
     }
 }
 
@@ -55,9 +100,17 @@ int main(void)
     stdio_init_all();
     msg_queue = xQueueCreate(100, sizeof(struct can2040_msg));
     canbus_setup();
-    TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread",
-                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1UL, &task);
+    TaskHandle_t trans_noise_task_h;
+    TaskHandle_t trans_data_task_h;
+    TaskHandle_t receive_task_h;
+    if(PICO_ID == 1)
+        xTaskCreate(transmit_noise_task, "NoiseThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 5UL, &trans_noise_task_h);
+    if(PICO_ID == 0)
+        xTaskCreate(transmit_data_task, "DataThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 0UL, &trans_data_task_h);
+    xTaskCreate(receive_task, "ReceiveThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 0UL, &receive_task_h);
     vTaskStartScheduler();
     return 0;
 }
